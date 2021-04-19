b10f26eb0dcf8cc25cc94e80c8f975ca23e801db
xiaobuisme <861954469@qq.com>
2021-04-19 18:38:41 +0800

    Add files via upload

---
+++ springboot-demo/src/main/java/com/example/springboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
